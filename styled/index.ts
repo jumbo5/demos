a66a42c0459bd2589ebc43a0281aaa9784ce314f
Юрий Лисецкий <liu@MacBook-Pro-Urij.local>
2020-07-18 21:50:19 +0700

    Add media endpoints

---
+++ styled/index.ts
@@ -0,0 +1,12 @@
+import mediaHelper from 'styled-media-helper'
+
+export const media = mediaHelper({
+  xxs: 320,
+  xs: 480,
+  s: 768,
+  sm: 1024,
+  lm: 1280,
+  lg: 1440,
+  xl: 1680,
+  xxl: 1920,
+})
