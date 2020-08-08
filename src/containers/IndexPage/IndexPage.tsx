aa7dc24ef4e3b201f8d175c3eaf22874cdd0d75a
Юрий Лисецкий <liu@MacBook-Pro-Urij.local>
2020-08-09 02:55:49 +0700

    Minor

--- src/containers/IndexPage/IndexPage.tsx
+++ src/containers/IndexPage/IndexPage.tsx
@@ -106,7 +106,7 @@ const List = styled.ul`
 
 const AlbumsContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, minmax(0, 1fr));
+  grid-template-columns: repeat(4, minmax(0, 1fr));
   gap: 32px;
 `
 
